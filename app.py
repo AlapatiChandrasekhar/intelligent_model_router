02bbc3c7d63fd94012fcdb756472dd79f1b842fd
AlapatiChandrasekhar <chandualapati44444@gmail.com>
2026-01-21 19:30:12 +0530

    Initial setup: Added app and logic files

---
+++ app.py
@@ -0,0 +1,7 @@
+# ==========================================
+# FRONTEND LAYER (Streamlit UI)
+# Responsibility: Handles user inputs and displays results.
+# ==========================================
+import streamlit as st
+import logic 
+...
---
+++ logic.py
@@ -0,0 +1,6 @@
+# ==========================================
+# BACKEND LAYER (Business Logic)
+# Responsibility: Routing, API calls, and Green Math calculations.
+# ==========================================
+
+...
